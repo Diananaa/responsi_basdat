59549c9ed2c55bbca2465314afb007e9b052da66
Diana <d1anasite04@gmail.com>
2020-11-21 13:32:54 +0700

    keranjang

--- admin/index.php
+++ admin/index.php
@@ -103,6 +103,25 @@
 
                             </ul>
                         </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-book"></i>
+                                <p>
+                                    Penjualan
+                                    <i class="fas fa-angle-left right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="page/keranjang.php" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Keranjang</p>
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </li>
                             
                 </nav>
                 <!-- /.sidebar-menu -->
